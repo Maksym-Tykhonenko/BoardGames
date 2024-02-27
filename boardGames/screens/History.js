a3cac775836d7e76e2d9763370946ffb08874fb4
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-02-27 16:36:03 +0200

    save

--- boardGames/screens/History.js
+++ boardGames/screens/History.js
@@ -1,10 +1,133 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  ImageBackground,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  Modal,
+} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const HistoryScreen = ({navigation}) => {
+  const [sideBarIsVisible, setSideBarIsVisible] = useState(false);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>HistoryScreen !!!</Text>
+    <View style={{flex: 1}}>
+      <ImageBackground
+        source={require('../assets/imgonline-com-ua-Resize-sTuE6CAxqAd.jpg')}
+        style={{flex: 1}}>
+        <SafeAreaView>
+          {/**SIDEBAR BTN */}
+          <View style={{}}>
+            <TouchableOpacity
+              style={{
+                width: 60,
+                height: 60,
+                backgroundColor: 'rgba(128, 128, 128, 0.4)',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+              onPress={() => {
+                setSideBarIsVisible(true);
+              }}>
+              <AntDesign name="menu-fold" style={{fontSize: 40}} />
+            </TouchableOpacity>
+          </View>
+
+          <Text>HistoryScreen !!!</Text>
+
+          {/**SIDEBAR */}
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={sideBarIsVisible}>
+            <View
+              style={{
+                backgroundColor: '#01aade',
+                flex: 1,
+                marginRight: '30%',
+                borderRightColor: '#fff',
+                borderWidth: 1,
+                borderTopRightRadius: 10,
+                borderBottomRightRadius: 10,
+              }}>
+              {/**BTN route & close block */}
+              <View style={{marginTop: 70, marginLeft: 20}}>
+                {/**BTN SideBar Close */}
+                <TouchableOpacity
+                  onPress={() => {
+                    setSideBarIsVisible(false);
+                  }}
+                  style={{marginBottom: 10}}>
+                  <Text
+                    style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                    X
+                  </Text>
+                </TouchableOpacity>
+
+                {/**BTN SideBar Route */}
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      marginBottom: 10,
+                    }}
+                    onPress={() => {
+                      navigation.navigate('Home');
+                      setSideBarIsVisible(false);
+                    }}>
+                    <Text
+                      style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                      Home
+                    </Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={{marginBottom: 10}}
+                    onPress={() => {
+                      navigation.navigate('ProfileScreen');
+                      setSideBarIsVisible(false);
+                    }}>
+                    <Text
+                      style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                      Profile
+                    </Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={{
+                      marginBottom: 10,
+                      borderBottomWidth: 1,
+                      borderColor: '#fff',
+                      width: 140,
+                    }}
+                    onPress={() => {
+                      navigation.navigate('HistoryScreen');
+                      setSideBarIsVisible(false);
+                    }}>
+                    <Text
+                      style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                      History
+                    </Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={{marginBottom: 10}}
+                    onPress={() => {
+                      navigation.navigate('GamesScreen');
+                      setSideBarIsVisible(false);
+                    }}>
+                    <Text
+                      style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                      Games
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+        </SafeAreaView>
+      </ImageBackground>
     </View>
   );
 };
--- boardGames/screens/HomeScreen.js
+++ boardGames/screens/HomeScreen.js
@@ -1,32 +1,133 @@
-import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  ImageBackground,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  Modal,
+} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const HomeScreen = ({navigation}) => {
+  const [sideBarIsVisible, setSideBarIsVisible] = useState(false);
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>HomeScreen !!!</Text>
-      <View>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('ProfileScreen');
-          }}>
-          <Text>Profile</Text>
-        </TouchableOpacity>
+    <View style={{flex: 1}}>
+      <ImageBackground
+        source={require('../assets/imgonline-com-ua-Resize-sTuE6CAxqAd.jpg')}
+        style={{flex: 1}}>
+        <SafeAreaView style={{flex: 1, marginHorizontal: 10}}>
+          {/**SIDEBAR BTN */}
+          <View style={{}}>
+            <TouchableOpacity
+              style={{
+                width: 60,
+                height: 60,
+                backgroundColor: 'rgba(128, 128, 128, 0.4)',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+              onPress={() => {
+                setSideBarIsVisible(true);
+              }}>
+              <AntDesign name="menu-fold" style={{fontSize: 40}} />
+            </TouchableOpacity>
+          </View>
 
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('HistoryScreen');
-          }}>
-          <Text>History</Text>
-        </TouchableOpacity>
+          {/**TEXT */}
+          <View>
+            <Text style={{fontSize: 25}}>HELLO IT`S APP ABOUT ....</Text>
+          </View>
 
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('GamesScreen');
-          }}>
-          <Text>Game</Text>
-        </TouchableOpacity>
-      </View>
+          {/**SIDEBAR */}
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={sideBarIsVisible}>
+            <View
+              style={{
+                backgroundColor: '#01aade',
+                flex: 1,
+                marginRight: '30%',
+                borderRightColor: '#fff',
+                borderWidth: 1,
+                borderTopRightRadius: 10,
+                borderBottomRightRadius: 10,
+              }}>
+              {/**BTN route & close block */}
+              <View style={{marginTop: 70, marginLeft: 20}}>
+                {/**BTN SideBar Close */}
+                <TouchableOpacity
+                  onPress={() => {
+                    setSideBarIsVisible(false);
+                  }}
+                  style={{marginBottom: 10}}>
+                  <Text
+                    style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                    X
+                  </Text>
+                </TouchableOpacity>
+
+                {/**BTN SideBar Route */}
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      marginBottom: 10,
+                      borderBottomWidth: 1,
+                      borderColor: '#fff',
+                      width: 140,
+                    }}
+                    onPress={() => {
+                      navigation.navigate('Home');
+                      setSideBarIsVisible(false);
+                    }}>
+                    <Text
+                      style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                      Home
+                    </Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={{marginBottom: 10}}
+                    onPress={() => {
+                      navigation.navigate('ProfileScreen');
+                      setSideBarIsVisible(false);
+                    }}>
+                    <Text
+                      style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                      Profile
+                    </Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={{marginBottom: 10}}
+                    onPress={() => {
+                      navigation.navigate('HistoryScreen');
+                      setSideBarIsVisible(false);
+                    }}>
+                    <Text
+                      style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                      History
+                    </Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={{marginBottom: 10}}
+                    onPress={() => {
+                      navigation.navigate('GamesScreen');
+                      setSideBarIsVisible(false);
+                    }}>
+                    <Text
+                      style={{color: '#fff', fontSize: 40, fontWeight: 'bold'}}>
+                      Games
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+        </SafeAreaView>
+      </ImageBackground>
     </View>
   );
 };
